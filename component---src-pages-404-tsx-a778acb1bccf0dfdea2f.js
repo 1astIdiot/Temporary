9a9312d0a8b3f1441857e2b894b800ce66d7f4f3
Evgeny Grigorev <evgen-grit@mail.ru>
2021-08-20 15:40:33 +0500

    Initial commit

---
+++ component---src-pages-404-tsx-a778acb1bccf0dfdea2f.js
@@ -0,0 +1,2 @@
+(self.webpackChunkrobotic=self.webpackChunkrobotic||[]).push([[218],{938:function(t,u,n){"use strict";n.r(u);n(7294);var l=n(5444),o=n(1319),e=n(8577),c=n(3431);u.default=function(){return(0,c.tZ)(o.Z,null,(0,c.tZ)(e.T3,null,(0,c.tZ)(e.W2,null,(0,c.tZ)("h1",null,"404: Page not found."),(0,c.tZ)("p",null,"You've hit the void. ",(0,c.tZ)(l.rU,{to:"/"},"Go back.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-a778acb1bccf0dfdea2f.js.map
